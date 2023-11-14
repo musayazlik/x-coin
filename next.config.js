41740a4267adc635d44702746cff1c6d6c3ef15e
Musa Yazlık <musayazlik1997@gmail.com>
2023-11-14 15:59:51 +0300

    Dil hatası giderildi.

--- next.config.js
+++ next.config.js
@@ -1,19 +1,18 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 const nextConfig = {
-
-  i18n: {
-    locales: ["en", "tr"],
-    defaultLocale: "en",
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
-
-
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
+	i18n: {
+		locales: ["en", "tr"],
+		defaultLocale: "en",
+		localeDetection: true,
+	},
+	sassOptions: {
+		includePaths: [path.join(__dirname, "styles")],
+	},
+	productionBrowserSourceMaps: false,
+	images: {
+		domains: ["res.cloudinary.com"],
+	},
 };
 
 module.exports = nextConfig;
