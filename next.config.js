806952119e1182c51b206ed656f30d89643284f9
Musa Yazlık <musayazlik1997@gmail.com>
2023-11-17 01:50:52 +0300

    Hatalar giderildi.

--- next.config.js
+++ next.config.js
@@ -1,33 +1,17 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 const nextConfig = {
+	i18n: {
+		locales: ["en", "tr"],
+		defaultLocale: "en",
+	},
+	sassOptions: {
+		includePaths: [path.join(__dirname, "styles")],
+	},
 
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'http://www.traderedit.com http://traderedit.com', // İzin verilen kök etki alanları
-          },
-        ],
-      },
-    ];
-  },
-
-  i18n: {
-    locales: ["en", "tr"],
-    defaultLocale: "en",
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
-
-
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
+	images: {
+		domains: ["res.cloudinary.com"],
+	},
 };
 
 module.exports = nextConfig;
