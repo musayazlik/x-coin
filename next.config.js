304a080c55c721b81dcb17659ec30274fca63115
root <root@VPS-MACHINE.kn300kvraeoe3pfmo5wkongasd.bx.internal.cloudapp.net>
2023-11-16 22:12:52 +0000

    dfsd

--- next.config.js
+++ next.config.js
@@ -5,22 +5,15 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
-        source: "/api/:path*",
+        source: '/(.*)',
         headers: [
-          {key: "Access-Control-Allow-Credentials", value: "true"},
-          {key: "Access-Control-Allow-Origin", value: "*"}, // replace this your actual origin
           {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT"
+            key: 'Access-Control-Allow-Origin',
+            value: 'http://www.traderedit.com http://traderedit.com', // İzin verilen kök etki alanları
           },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-          },
-        ]
-      }
-    ]
+        ],
+      },
+    ];
   },
 
   i18n: {
